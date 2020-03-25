ea6fa766909b0bf00fc8ebe2f3230d757d95cba2
joel quispe <joelquispeunju@gmail.com>
2020-03-24 21:34:08 -0300

    add README

---
+++ README.md
@@ -0,0 +1,16 @@
+# cuilCalculator
+
+Un pequeño script para obtener el cuil de un lote de datos. Para ejecutar el mismo, debe clonar el repositorio, almacenar en la raiz del mismo el archivo .csv en el que se encuentre los datos necesarios y ejecutar el siguiente comando:
+
+```
+python3 readWriteCsv.py
+
+```
+
+La estructura del archivo, que debe ser nombrado como test.csv, deberá ser la siguiente:
+
+```
+dni, sexo
+```
+
+El resultado, es un nuevo archivo .csv, denominado result.csv, en la raiz del repositorio.
--- readWriteCsv.py
+++ readWriteCsv.py
@@ -2,43 +2,45 @@ import csv
 
 cuilMultiplicators = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
 
-def completeDni(value):
-    return ''.join(['0', value])
-
 def completeCuil(previusCuil, rest):
 
     if rest == 0:
-        return previusCuil + '0'
+        return previusCuil[0:2] + '-' + previusCuil[2:] + '-0'
     elif rest == 1:
         if previusCuil[0:2] == '20':
-            return '23' + previusCuil[2:] + '9'
+            return '23-' + previusCuil[2:] + '-9'
         if previusCuil[0:2] == '27':
-            return '23' + previusCuil[2:] + '4'
+            return '23-' + previusCuil[2:] + '-4'
         
-    return previusCuil + str(11 - rest)
+    return previusCuil[0:2] + '-' + previusCuil[2:] + '-' + str(11 - rest)
 
-with open('result.csv', newline='') as File:
+with open('test.csv', newline='') as File:
     reader = csv.reader(File)
 
     newData = []
 
     for data in reader:
         if data[0] != 'dni':
-            
-            dni = completeDni(data[0]) if (len(data[0]) < 8) else data[0]
-            previusCuil = '20' + dni if (data[1] == 'M') else '27' + dni
 
-            acumulator = 0
-            for element, digit in zip(cuilMultiplicators, previusCuil):
-                acumulator += element * int(digit)
+            if len(data[0]) == 7 or len(data[0]) == 8:
 
-            data[2] = completeCuil(previusCuil, acumulator % 11)
+                dni = '0' + data[0] if (len(data[0]) < 8) else data[0]
+                previusCuil = '20' + dni if (data[1] == 'M') else '27' + dni
 
-            newData.append(data)
+                acumulator = 0
+                for element, digit in zip(cuilMultiplicators, previusCuil):
+                    acumulator += element * int(digit)
+
+                data[2] = completeCuil(previusCuil, acumulator % 11)
+
+                newData.append(data)
+            else:
+                newData.append(data)
+
         else:
             newData.append(data)
 
-    with open('test.csv', 'w') as writeFile:
+    with open('result.csv', 'w') as writeFile:
 
         writer = csv.writer(writeFile, delimiter=',')
         writer.writerows(newData)
